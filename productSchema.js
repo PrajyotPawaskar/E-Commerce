eddc0f9ac066119c47de33cee690309a5070aa4f
Prajyot Pawaskar <prajyotpawaskar2001@gmail.com>
2024-09-01 19:19:26 +0530

    Update productSchema.js

--- productSchema.js
+++ productSchema.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  id: Number,
-  title: String,
-  price: Number,
-  description: String,
-  category: String,
-  image: String,
-  rating: {
-    rate: Number,
-    count: Number,
-  },
-});
+// const productSchema = new mongoose.Schema({
+//   id: Number,
+//   title: String,
+//   price: Number,
+//   description: String,
+//   category: String,
+//   image: String,
+//   rating: {
+//     rate: Number,
+//     count: Number,
+//   },
+// });
 
-const Products = mongoose.model("products", productSchema);
+// const Products = mongoose.model("products", productSchema);
 
-export default Products;
+// export default Products;
