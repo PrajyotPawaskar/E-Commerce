6429b529cab36419fae43b737c82fab7609c7f05
Prajyot Pawaskar <prajyotpawaskar2001@gmail.com>
2024-09-01 19:19:35 +0530

    Update userSchema.js

--- userSchema.js
+++ userSchema.js
@@ -1,30 +1,30 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const cartItemSchema = new mongoose.Schema({
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Products", // Assuming you have a Product model
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    default: 0,
-  },
-});
+// const cartItemSchema = new mongoose.Schema({
+//   productId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Products", // Assuming you have a Product model
+//     required: true,
+//   },
+//   quantity: {
+//     type: Number,
+//     default: 0,
+//   },
+// });
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  cart: [cartItemSchema], // Add the cart field using the cartItemSchema
-});
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   cart: [cartItemSchema], // Add the cart field using the cartItemSchema
+// });
 
-const Users = mongoose.model("users", userSchema);
+// const Users = mongoose.model("users", userSchema);
 
-export default Users;
+// export default Users;
